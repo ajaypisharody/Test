6f159db46cd45e31c7c78f6e6018b3118e9907e8
Ajay Pisharody <43316414+ajaypisharody@users.noreply.github.com>
2025-05-04 22:26:07 +0530

    Update installed_base.py

--- frontend/modules/installed_base.py
+++ frontend/modules/installed_base.py
@@ -118,8 +118,7 @@ def render_installed_base():
     with st.expander("📊 Equipment Overview"):
         st.plotly_chart(px.histogram(data, x="Usage Hours"))
         st.plotly_chart(px.pie(data, names="Service History", title="Service Distribution"))
-        st.plotly_chart(px.bar(data["Location"].value_counts().reset_index(),
-                               x=data.index, y="Location", title="Units per Location"))
+        #st.plotly_chart(px.bar(data["Location"].value_counts().reset_index(),x=data.index, y="Location", title="Units per Location"))
 
     # Entitlement Estimation
     with st.expander("📐 Entitlement"):
